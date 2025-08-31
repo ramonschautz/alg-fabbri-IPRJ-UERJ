6fdb44173fbd8c38146fb498884278474f0c1c82
Ricardo Fabbri <rfabbri@gmail.com>
2025-08-31 15:50:54 -0300

    linked list stub from last aula pratica

---
+++ alg/merge/cmd/linked_list_string-cmd.cpp
@@ -0,0 +1,14 @@
+/* R. Fabbri, 2024 */
+#include <iostream>
+#include <linked_list_string.h>
+
+int
+main()
+{
+  std::cout << "Lista ---------------------------------------------------------\n";
+  playground_lista();
+  std::cout << "Lista2 ---------------------------------------------------------\n";
+  playground_lista2();
+
+  return 0;
+}
